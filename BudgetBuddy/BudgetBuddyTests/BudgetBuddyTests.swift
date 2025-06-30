d70d331454b8386b4b6f8c91d247feac9d9b3275
mdadil27 <120901555+mdadil27@users.noreply.github.com>
2025-06-30 14:19:34 +0530

    Add files via upload

---
+++ BudgetBuddy/BudgetBuddyTests/BudgetBuddyTests.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetBuddyTests.swift
+//  BudgetBuddyTests
+//
+//  Created by Md Adil on 24/05/25.
+//
+
+import Testing
+@testable import BudgetBuddy
+
+struct BudgetBuddyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
